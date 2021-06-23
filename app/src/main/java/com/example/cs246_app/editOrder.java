606e7402a5541d6d96fab21836eb82c11f28addd
Yoli-Buendia <bue17003@byui.edu>
2021-06-23 18:32:13 -0500

    after the tutoring sesssion

--- app/src/main/java/com/example/cs246_app/editOrder.java
+++ app/src/main/java/com/example/cs246_app/editOrder.java
@@ -1,39 +1,40 @@
 package com.example.cs246_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class editOrder extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class editOrder extends AppCompatActivity {
+    OrderAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_order);
-        displayItem();
+        //displayItem();
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        //set the adapter
+        RecyclerView view = findViewById(R.id.recycler_orders);
+        adapter = new OrderAdapter(this, animalNames);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
     }
 
-    int item = 0;
 
-    public void addItem(View view){
-        item++;
-        displayQuantity();
-    }
 
-    public void decreaseItem(View view){
-        item--;
-        displayQuantity();
-    }
 
-    public void displayQuantity(){
-        //find view by id
-        EditText text = findViewById(R.id.quantityBtn);
-        //set text to it
-        text.setText(String.valueOf(item));
-
-    }
 
     public void displayItem(){
         String foodItem = "Burger";
