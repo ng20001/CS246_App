959b180b31998c266028a3d7c2447645f1c78cfd
Yoli-Buendia <bue17003@byui.edu>
2021-06-23 21:42:37 -0500

    pulling before ccomitting

--- app/src/main/java/com/example/cs246_app/backOfHouse.java
+++ app/src/main/java/com/example/cs246_app/backOfHouse.java
@@ -1,14 +1,44 @@
 package com.example.cs246_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class backOfHouse extends AppCompatActivity {
-
+    OrderAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_back_of_house);
+        setContentView(R.layout.activity_edit_order);
+        //displayItem();
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+
+        //set the adapter
+        RecyclerView view = findViewById(R.id.recycler_orders);
+        adapter = new OrderAdapter(this, animalNames);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+    }
+
+
+
+
+
+    public void displayItem(){
+        String foodItem = "Burger";
+        EditText text = findViewById(R.id.foodItemDisplay2);
+        //set text to it
+        text.setText(foodItem);
     }
 }
