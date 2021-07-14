e1b7007a6f612aa53244071e702aa613852fe724
ng20001 <ng20001@byui.edu>
2021-07-14 23:14:32 +0900

    Trying on creating wifi p2p class

---
+++ app/src/main/java/com/example/cs246_app/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,36 @@
+//package com.example.cs246_app;
+//
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.net.wifi.p2p.WifiP2pManager;
+//
+//// Testing on WiFi P2P api
+//public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+//    private WifiP2pManager manager;
+//    private WifiP2pManager.Channel channel;
+//    private MyWiFiActivity activity;
+//
+//    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+//                                       MyWifiActivity activity) {
+//        super();
+//        this.manager = manager;
+//        this.channel = channel;
+//        this.activity = activity;
+//    }
+//
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        String action = intent.getAction();
+//
+//        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+//            // Check to see if Wi-Fi is enabled and notify appropriate activity
+//        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+//            // Call WifiP2pManager.requestPeers() to get a list of current peers
+//        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+//            // Respond to new connection or disconnections
+//        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+//            // Respond to this device's wifi state changing
+//        }
+//    }
+//}
